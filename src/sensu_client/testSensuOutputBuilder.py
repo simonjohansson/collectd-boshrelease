ee64b6ceb19403d6e28498e5c984bba5ef36613e
Matt Johnson <matjohn2@cisco.com>
2014-10-03 11:58:27 +0100

    initial changes for story 79696750 - pending testing

--- src/sensu_client/testSensuOutputBuilder.py
+++ src/sensu_client/testSensuOutputBuilder.py
@@ -13,6 +13,7 @@ class TestSequenceFunctions(unittest.TestCase):
         self.sensu_healthy_job_output = {'status' : 'running'}
         self.sensu_healthy_json_output='{"status": 0, "name": "test job", "handlers": ["twitter"], "monit data status": "running", "subscribers": "cfmonit", "output": "Monit Job test job is running", "type": "monit"}'
         self.sensu_unhealthy_job_output = {'status' : 'not monitored'}
+        self.sensu_pending_action_job_output = {'status' : 'restart pending'}
 
     def test_sensu_output_contains_the_correct_data(self):
         """
@@ -51,6 +52,15 @@ class TestSequenceFunctions(unittest.TestCase):
         sensu_output_dead_json = json.loads(sensu_output_dead)
         self.assertTrue('status' in sensu_output_dead_json)
         self.assertEqual(sensu_output_dead_json['status'], 2)
+
+    def test_sensu_output_returns_warning_status_for_pending_jobs(self):
+        """
+        Make sure the summary parser returns the correct number of jobs
+        """
+        sensu_output_pending = SensuOutputBuilder().create_output_for_job('test job', self.sensu_pending_action_job_output)
+        sensu_output_pending_json = json.loads(sensu_output_pending)
+        self.assertTrue('status' in sensu_output_pending_json)
+        self.assertEqual(sensu_output_pending_json['status'], 1)
 
 if __name__ == '__main__':
     unittest.main()
